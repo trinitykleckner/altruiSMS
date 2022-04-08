f5a1a4958adee485455a20d02e4d143f3f99b7b4
tkleckner <tkleckner@haverford.edu>
2022-04-08 17:22:51 -0400

    ran requirements commands

--- codefest/altruiSMS/views.py
+++ codefest/altruiSMS/views.py
@@ -54,7 +54,7 @@ def sms(request):
     added = []
     print(incoming)
     if new(to_number):
-        r = "Hi! I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future. (To change language text \"language\")"
+        r = "1Hi! I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future. (for a different language type \"language\")" #(para mensajes en espanol, texto \"espanol\")"
         person = Beneficiary.objects.create(phone_num=to_number)
         person.save()
     else:
@@ -63,18 +63,25 @@ def sms(request):
             incoming = ts.google(incoming, to_language='en')
         if "help me" in incoming:
             r = help_menu()
-        if "hi" or "hey" or "hello" in incoming:
-            r = "Hi!  I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future. (para mensajes en espanol, texto espanol)"
+        if "hi" == incoming or "hey" == incoming or "hello" == incoming:
+            r = "2Hi!  I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future."
         if "items" in incoming:
             r += "Here are the items you can opt into (you can do so by replying with the names of the items you would like to be notified about):\n"+items[0]
             for item in items[1:]:
                 r += ", "+item
-        if "espanol" in incoming:
-            person.language = "es"
-            r = "Hi!  I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future."
-        if "english" in incoming:
-            person.language = "en"
-            r = "Hi!  I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future."
+        if "language" in incoming:
+            r += "what language? english, spanish, french, chinese, arabic, russian, german, italian, japanese, or korean"
+        for lang in lang_dict.keys():
+            if lang in incoming and lang != "english":
+                person.language = lang_dict[lang]
+                r = "3Hi!  I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future."
+
+        # if "espanol" in incoming:
+        #     person.language = "es"
+        #     r = "Hi!  I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future."
+        # if "english" in incoming:
+        #     person.language = "en"
+        #     r = "Hi!  I’m chatbot, I am here to help you get access to the resources you need. By responding to this text you can opt into receiving notifications as local organizations are holding drives, or giving away specific items. The items you can choose to be notified about are food, diapers, blankets, and sanitary items. Reply to this text with the names of the items you wish to be notified about in the future."
         if "food" in incoming:
             if "remove food" in items:
                 removed.append("food")
@@ -149,7 +156,7 @@ def sms(request):
                     road2 = ' '.join(split[split.index(word) + 1:])
                     set_location(person, road1, road2)
                     r += "Thanks!! You will now be notified when there is a distribution near you.\n"
-                    r += "Coords: ["+str(person.latitude)+','+str(person.longitude)+']'
+                    #r += "Coords: ["+str(person.latitude)+','+str(person.longitude)+']'
                     found = True
                     break
             if not found:
@@ -317,7 +324,7 @@ class Login(View):
             return render(request, self.template, {'form': form})
 
 #returns true if num not in which phone list
-def new(num, which="master"):
+def new(num):
     if Beneficiary.objects.exists():
         return False
     else:
