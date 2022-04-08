97cf654f7a18fc3c9680d616ce4999653640d3cb
Samuel <samueltan97@hotmail.com>
2022-04-08 11:06:03 -0400

    Changed static strategy

--- codefest/codefest/settings.py
+++ codefest/codefest/settings.py
@@ -13,7 +13,18 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,'altruiSMS/static'),
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -77,7 +88,7 @@ WSGI_APPLICATION = 'codefest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + '/'+ 'db.sqlite3',
     }
 }
 
@@ -120,13 +131,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
-STATIC_URL = 'static/'
-#location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'altruiSMS/static')
-]
-print(os.path.join(BASE_DIR,'altruiSMS\static'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
